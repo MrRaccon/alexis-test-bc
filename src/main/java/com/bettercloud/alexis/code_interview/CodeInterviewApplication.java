c92119cd92ef7ec246d4f090bed4fd5705e6fc6d
Alexis Biñuelo <31171057+MrRaccon@users.noreply.github.com>
2024-07-03 18:57:32 -0600

    code base

---
+++ src/main/java/com/bettercloud/alexis/code_interview/CodeInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.bettercloud.alexis.code_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bettercloud/alexis/code_interview/CodeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettercloud.alexis.code_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
